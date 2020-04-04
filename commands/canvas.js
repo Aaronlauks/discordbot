35d41dfa3371457b1690dcada7b68ab7dcb19487
aaronlauks <aaronlauks01@gmail.com>
2020-04-04 19:55:39 +0800

    ok

--- commands/canvas.js
+++ commands/canvas.js
@@ -6,7 +6,7 @@ exports.run = async (bot, message, args, ops) => {
 	const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage('./wallpaper.jpg');
+	const background = await Canvas.loadImage('./bg.jpg');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	// Select the color of the stroke
