88130199eff133ae841d1ede38ffd1329ee3f2b4
aaronlauks <aaronlauks01@gmail.com>
2020-04-04 22:10:38 +0800

    ok

--- commands/canvas.js
+++ commands/canvas.js
@@ -1,13 +1,12 @@
 const { RichEmbed } = require("discord.js");
 const Discord = require("discord.js");
 const Canvas = require('canvas');
-const bg = require('../bg.jpg')
 
 exports.run = async (bot, message, args, ops) => {
 	const canvas = Canvas.createCanvas(700, 250);
 	const ctx = canvas.getContext('2d');
 
-	const background = await Canvas.loadImage(bg);
+	const background = await Canvas.loadImage('./bg.jpg');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
 	// Select the color of the stroke
@@ -18,7 +17,6 @@ exports.run = async (bot, message, args, ops) => {
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
 	message.channel.send(`cool picture test`, attachment);
-	message.channel.send(`cool gay test`, bg);
 }
 
 module.exports.config = {
