23b21d0c8858342c16546897f31630ddbfc4c350
aaronlauks <aaronlauks01@gmail.com>
2020-05-06 14:20:19 +0800

    pls work

--- commands/mock.js
+++ commands/mock.js
@@ -1,24 +1,30 @@
 const Discord = require("discord.js");
 const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
 
-exports.run = (bot, message, args) => {
-    if (args.length < 1) return message.channel.send("<:xcross:658850997757804555> Please provide some text to Mock")
-
-    let mockEmbed = new Discord.RichEmbed()
-    .setColor("#00ff00")
-    .setDescription(args.map(randomizeCase))
-    .setThumbnail("https://cdn.discordapp.com/attachments/424889733043191810/425242569325150208/mock.jpg")
-
-    message.channel.send(mockEmbed)
-
-    message.delete();
-
+exports.run = async (bot, message, args) => {
+    let num = 0;
+    if (args.length < 1) {
+      fetched = await message.channel.fetchMessages({limit: 2});
+      let a = [];
+      fetched.forEach(f => {
+        num++;
+        a.push(f.author.username);
+        a.push(f.content.split(/ +/g));
+        console.log(f.content);
+        if(num == 2){
+          if(f.content == "" || f.content == null) return message.channel.send(`<:xcross:690880230562201610> There is no message above you xC`);
+          message.channel.send(`${a[3].map(randomizeCase).join(" ")}`);
+        }
+      });
+    } else {
+      message.channel.send(args.map(randomizeCase));
+    }
 }
 
 
 module.exports.config = {
   name: "mock",
-  description: "Does the spongebob chicken mock and makes your words LiKe tHiS wHicH iS gAy",
+  description: "Does the spongebob chicken mock and makes your words or the previous message LiKe tHiS wHicH iS gAy",
   category: "Fun",
   usage: "a mock <message>",
   accessableby: "Everyone",
--- commands/reverse.js
+++ commands/reverse.js
@@ -1,18 +1,20 @@
 exports.run = async function (bot, message, args) {
+  let num = 0;
     if (args.length < 1) {
       fetched = await message.channel.fetchMessages({limit: 2});
       let a = [];
       fetched.forEach(f => {
+        num++;
         a.push(f.author.username);
         a.push(f.content.split('').reverse().join(''));
-        console.log(f.content);
-        if(f.content == "" || f.content == null) return message.channel.send(`<:xcross:690880230562201610> There is no message above you xC`)
-      })
-      if(a[2] && a[3]){
-        message.channel.send(`**${a[2]}:** ${a[3]}`)
-      } else return message.channel.send(`<:xcross:690880230562201610> There is no message above you xC`)
+        if(num == 2){
+          if(f.content == "" || f.content == null) return message.channel.send(`<:xcross:690880230562201610> There is no message above you xC`);
+          message.channel.send(`${a[3]}`);//**${a[2]}:** FOR USERNAME
+        }
+      });
+    } else {
+      message.channel.send(args.join(' ').split('').reverse().join(''));
     }
-    message.channel.send(args.join(' ').split('').reverse().join(''));
 }
 
 module.exports.config = {
